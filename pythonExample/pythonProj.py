6daa726558191d3e675d4aae02759430a23437c4
Carmit Hershman <carmith@jfrog.com>
2026-02-26 10:25:30 +0200

    initial commit

---
+++ pythonExample/pythonProj.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# adding a faewture in CTOA-1010, commit 1 
+# adding a faewture in CTOA-1010, commit 2
+
+# Function definition is here
+def printme( str ):
+    # This prints a passed string into this function
+    print (str)
+    return;
+
+from project import db, app
+from Cryptodome.Cipher import ARC4
+import hashlib
+
+def arc4_encrypt_password(key, password):
+    cipher = ARC4.new(key.encode('utf-8'))
+    encrypted_password = cipher.encrypt(password.encode('utf-8'))
+    return hashlib.md5(encrypted_password).hexdigest()
+
+# Now you can call printme function
+printme("Hello from JFROG!!");
+printme("I am part of test skill 4 jira!!");
+printme("I am part of test skill 4 commit 2");
+printme("I am part of test skill 4 commit 3 which is on jira 1014");
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='jfrog-test-example2',
+    version='1.1',
+    description='Project example for building Python project with JFrog products.',
+    author='JFrog',
+    author_email='jfrog@jfrog.com',
+    url='https://github.com/carmithersh/carmit-testing',
+    packages=['pythonExample'],
+    install_requires=['PyYAML>3.11'],
+)
